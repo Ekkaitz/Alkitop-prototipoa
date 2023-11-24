42b3c6ab50a2812587ae69da4bb25c2284d7e838
Ekkaitz <erubioel22wg@ikzubirimanteo.com>
2023-11-24 12:50:54 +0100

    Menua

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,26 @@ import './App.css'
 import { Header } from './assets/components/Header'
 import { IconMenu } from './assets/components/IconMenu'
 import 'bootstrap/dist/css/bootstrap.css'
+
 function App () {
   return (
     <>
       <Header />
-      <IconMenu />
+
+      <div className='row pt-4 '>
+        <IconMenu />
+        <IconMenu />
+        <IconMenu />
+        <IconMenu />
+      </div>
+
+      <div className='row pt-4 '>
+        <IconMenu />
+        <IconMenu />
+        <IconMenu />
+        <IconMenu />
+      </div>
+
     </>
   )
 }
--- client/src/assets/components/IconMenu.jsx
+++ client/src/assets/components/IconMenu.jsx
@@ -6,30 +6,9 @@ import controller from '../svg/controller.svg'
 
 export const IconMenu = () => {
   return (
-    <>
-      <div className='row pt-4 '>
-        <a href='#' className='col text-center'>
-          <img src={plane} alt='' className='w-25' />
-        </a>
+    <a href='#' className='col text-center'>
+      <img src={plane} alt='' className='w-25' />
+    </a>
 
-        <a href='#' className='col text-center'>
-          <img src={book} alt='' className='w-25' />
-        </a>
-
-        <a href='#' className='col text-center'>
-          <img src={scooter} alt='' className='w-25' />
-        </a>
-
-        <a href='#' className='col text-center'>
-          <img src={camera} alt='' className='w-25' />
-        </a>
-
-        <a href='#' className='col text-center'>
-          <img src={controller} alt='' className='w-25' />
-        </a>
-
-      </div>
-
-    </>
   )
 }
