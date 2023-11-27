ffa298f70e029165ea99d558401dd9ffaf81c931
ekaru <erubioel22wg@ikzubirimanteo.com>
2023-11-27 20:29:14 +0100

    AAA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,36 @@
-import './App.css'
-import { Header } from './assets/components/Header'
-import { IconMenu } from './assets/components/IconMenu'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import { Header } from './assets/components/Header';
+import { IconMenu } from './assets/components/IconMenu';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App () {
+import plane from './assets/svg/plane.svg';
+import book from './assets/svg/book.svg';
+import scooter from './assets/svg/scooter.svg';
+import camera from './assets/svg/camera.svg';
+import controller from './assets/svg/controller.svg'
+import bicycle from './assets/svg/bicycle.svg'
+import car from './assets/svg/car.svg'
+
+function App() {
   return (
     <>
       <Header />
 
       <div className='row pt-4 '>
-        <IconMenu />
-        <IconMenu />
-        <IconMenu />
-        <IconMenu />
+        <IconMenu image={plane} href='#' />
+        <IconMenu image={book} href='#' />
+        <IconMenu image={scooter} href='#' />
+        <IconMenu image={camera} href='#' />
       </div>
 
       <div className='row pt-4 '>
-        <IconMenu />
-        <IconMenu />
-        <IconMenu />
-        <IconMenu />
+        <IconMenu image={controller} href='#' />
+        <IconMenu image={bicycle} href='#' />
+        <IconMenu image={car} href='#' />
+        <IconMenu image={camera} href='#' />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/assets/components/IconMenu.jsx
+++ client/src/assets/components/IconMenu.jsx
@@ -1,13 +1,7 @@
-import plane from '../svg/plane.svg'
-import book from '../svg/book.svg'
-import scooter from '../svg/scooter.svg'
-import camera from '../svg/camera.svg'
-import controller from '../svg/controller.svg'
-
-export const IconMenu = () => {
+export const IconMenu = ({ image, href }) => {
   return (
-    <a href='#' className='col text-center'>
-      <img src={plane} alt='' className='w-25' />
+    <a href={href} className='col text-center'>
+      <img src={image} alt='' className='w-15' />
     </a>
 
   )
