03fe6c5dd78ae8d8659e28e6e3feebd1ea024441
Ekkaitz <erubioel22wg@ikzubirimanteo.com>
2023-11-28 08:15:25 +0100

    28

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Header } from './assets/components/Header';
-import { IconMenu } from './assets/components/IconMenu';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import { Header } from './assets/components/Header'
+import { IconMenu } from './assets/components/IconMenu'
+import 'bootstrap/dist/css/bootstrap.css'
 
-import plane from './assets/svg/plane.svg';
-import book from './assets/svg/book.svg';
-import scooter from './assets/svg/scooter.svg';
-import camera from './assets/svg/camera.svg';
+import plane from './assets/svg/plane.svg'
+import book from './assets/svg/book.svg'
+import scooter from './assets/svg/scooter.svg'
+import camera from './assets/svg/camera.svg'
 import controller from './assets/svg/controller.svg'
 import bicycle from './assets/svg/bicycle.svg'
 import car from './assets/svg/car.svg'
 
-function App() {
+function App () {
   return (
     <>
       <Header />
@@ -29,8 +29,13 @@ function App() {
         <IconMenu image={car} href='#' />
         <IconMenu image={camera} href='#' />
       </div>
+
+      <div className=' my-5 '>
+        <hr />
+      </div>
+
     </>
-  );
+  )
 }
 
-export default App;
+export default App
